db101237bcbe51055faf9c07ef94f8a31f22202d
shekinah <saambilishekinah4@gmail.com>
2023-04-14 06:57:53 -0700

    create sum function

--- prog.c
+++ prog.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
-int main(){
-printf("hello lab4\n");
+
+int find_sum(){
+
+	int a, b;
+	printf("enter the number: \n");
+	scanf("%d%d", &a,&b);
+	int somme= a+b;
+	printf("the sum is: %d\n", somme);
+
+	return 0;
+}
+
+int main (){
+	printf("hello lab4 \n");
+	find_sum();
 }
